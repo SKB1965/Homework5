0321a99f721e9ab8e538f44a0ff25f6e92932a01
stanley <skbeyam@dmacc.edu>
2019-02-20 22:20:07 -0600

    Homework5

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+package edu.dmacc.codedsm.hw5;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        double x = Math.sqrt(27);
+        System.out.println(x);
+     int k = 28/5;
+        System.out.println(k);
+        double y = Math.pow(2.2,4);
+        System.out.println(y);
+        double l = Math.random()*10;
+        System.out.println((int)l);
+    }
+}
